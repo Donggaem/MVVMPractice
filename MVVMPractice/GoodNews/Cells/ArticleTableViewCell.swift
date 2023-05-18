4d0fa87f0fd3d4df2c52bd067281c05f1cb3b046
Donggaem <ehdrua465111@naver.com>
2023-05-18 11:54:43 +0900

    뉴스앱 - 맞춤형 자체 크기 조정 셀 만들기

---
+++ MVVMPractice/GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  MVVMPractice
+//
+//  Created by 김동겸 on 2023/05/18.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
