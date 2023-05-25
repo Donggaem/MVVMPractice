b9b5dab17ce3d50330a0d3d981d976a9ab0c908a
Donggaem <ehdrua465111@naver.com>
2023-05-25 10:31:20 +0900

    커피 주문 앱 - 모델 만들기

---
+++ MVVMPractice/HotCoffee/Models/Order.swift
@@ -0,0 +1,29 @@
+//
+//  Order.swift
+//  MVVMPractice
+//
+//  Created by 김동겸 on 2023/05/25.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+}
