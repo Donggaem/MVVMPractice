e4f19b3eb85c9d8a9430abbca84e7f72cfe47c43
Donggaem <ehdrua465111@naver.com>
2023-05-26 21:14:55 +0900

    커피 주문 앱 - 웹 서비스 클라이언트 테스트

--- MVVMPractice/HotCoffee/Controllers/OrdersTableViewController.swift
+++ MVVMPractice/HotCoffee/Controllers/OrdersTableViewController.swift
@@ -10,4 +10,30 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        populateOrders()
+    }
+    
+    private func populateOrders() {
+        
+        guard let coffeeOrdersURL = URL(string: "https://warp-wiry-rugby.glitch.me/orders") else {
+            fatalError("URL was incorrect")
+            return
+        }
+        
+        let resource = Resource<[Order]>(url: coffeeOrdersURL)
+        
+        Webservice_Coffee().load(resource: resource) { result in
+            
+            switch result {
+            case .success(let orders):
+                print(orders)
+            case .failure(let error):
+                print(error)
+            }
+            
+        }
+    }
 }
