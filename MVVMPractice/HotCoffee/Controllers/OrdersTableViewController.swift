9485585a17fd9de44de0bd140431c1f43ebad2f9
Donggaem <ehdrua465111@naver.com>
2023-07-21 18:36:58 +0900

    커피 주문 앱 - UITableView에 주문 표시하기

--- MVVMPractice/HotCoffee/Controllers/OrdersTableViewController.swift
+++ MVVMPractice/HotCoffee/Controllers/OrdersTableViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    var orderListViewModel = OrdersListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         populateOrders()
     }
     
@@ -25,15 +26,36 @@ class OrdersTableViewController: UITableViewController {
         
         let resource = Resource<[Order]>(url: coffeeOrdersURL)
         
-        Webservice_Coffee().load(resource: resource) { result in
+        Webservice_Coffee().load(resource: resource) { [weak self] result in
             
             switch result {
             case .success(let orders):
-                print(orders)
+                self?.orderListViewModel.ordersViewModel = orders.map(OrderViewModel.init)
+                self?.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
             
         }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.orderListViewModel.ordersViewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let vm = self.orderListViewModel.orderViewModel(at: indexPath.row)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderTableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = vm.type
+        cell.detailTextLabel?.text = vm.size
+        
+        return cell
     }
 }
