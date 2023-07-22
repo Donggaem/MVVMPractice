4dbc8bb1d4950fb3563af2643d9fd891ff468c3a
Donggaem <ehdrua465111@naver.com>
2023-07-22 14:23:51 +0900

    커피 주문 앱 - 커피의 유형 (주문하기 테이블뷰 구현)

---
+++ MVVMPractice/HotCoffee/ViewModels/AddCoffeeOrderViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  AddCoffeeOrderViewModel.swift
+//  MVVMPractice
+//
+//  Created by 김동겸 on 2023/07/22.
+//
+
+import Foundation
+
+struct AddCoffeeOrderViewModel {
+    
+    var name: String?
+    var email: String?
+    
+    var types: [String] {
+        return CoffeeType.allCases.map {$0.rawValue.capitalized}
+    }
+    
+    var sizes: [String] {
+        return CoffeeSize.allCases.map { $0.rawValue.capitalized}
+    }
+}
