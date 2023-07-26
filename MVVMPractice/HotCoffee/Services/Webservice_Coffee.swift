c02e030e9560b72f3d327a415fa77ce2ca74cbb6
Donggaem <ehdrua465111@naver.com>
2023-07-26 14:42:40 +0900

    커피 주문 앱 - 게시물 추가를 위한 웹서비스 업데이트(request 구현)

--- MVVMPractice/HotCoffee/Services/Webservice_Coffee.swift
+++ MVVMPractice/HotCoffee/Services/Webservice_Coffee.swift
@@ -34,8 +34,12 @@ class Webservice_Coffee {
     
     func load<T>(resource: Resource<T>, completion: @escaping (Result<T, NetworkError>) -> Void) {
         
+        var request = URLRequest(url: resource.url)
+        request.httpMethod = resource.httpMethod.rawValue
+        request.httpBody = resource.body
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        URLSession.shared.dataTask(with: resource.url) { data, response, error in
+        URLSession.shared.dataTask(with: request) { data, response, error in
             
             guard let data = data, error == nil else {
                 completion(.failure(.DomainError))
